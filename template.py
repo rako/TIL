ad1e993ed4648396b249ede9812e1fe9bd4d0807
rako <=>
2024-10-13 18:49:03 +0900

    date.todayの扱いの修正とコメント追加

--- template.py
+++ template.py
@@ -1,11 +1,15 @@
 import datetime
 import os
 
-d_today = datetime.date.today()
-year = str(d_today.year)
-month = str(d_today.month)
-day = str(d_today.day)
+#classmethod datetime.date.today() 現在のローカルな日付を返す
+d_today = datetime.date.today().split('-') #2020-01-01の形式で返す
+year = str(d_today[0])
+month = str(d_today[1])
+day = str(d_today[2])
+#本当はd_today.dayでいいけど、日のファイル名が一桁の場合は0がつかないので10以降のファイルの後にソートされてしまうため、'-'で分けている。
+#dayのみ一桁の場合は0をつける条件にしても良いかもしれない。monthも同様になるので、これでいいかもしれない。
 
+#mdファイルのテンプレートの中身
 content = """
 # Today I Learned
 
@@ -18,12 +22,14 @@ content = """
 ## 参考文献リスト
 """
 
+
 #ディレクトリの作成を監視するフラグ, これないとfor文が回るごとにディレクトリが作成されてしまうかも？
 yearflag = False
 monthflag = False
 dayflag = False
 
-#カレントディレクトリのなかに年月のディレクトリがあるかと日のファイルがあるかどうかを確認
+#カレントディレクトリのなかに年月のディレクトリがあるかと日のファイルがあるかどうかを確認。該当するディレクトリとファイルがない場合は何もしないことにする。
+#match case文を使ったらいいかもしれない。
 for yearname in os.scandir(path='.'):
     if yearname.is_dir(): #yearnameがディレクトリでない場合はスキップ
         if yearname == year: #年のディレクトリが一緒の場合
@@ -47,7 +53,7 @@ if yearflag == False: #年のディレクトリがない場合
         pass #年のディレクトリがなくて月のディレクトリがある場合はないとする
 else: #年のディレクトリがある場合
     if monthflag == False: #月のディレクトリはない場合
-        os.chdir('yeardir')
+        os.chdir('yeardir') #ここでディレクトリ移動しないで済む方法があるかもしれない
         os.mkdir('monthdir')
     else:
         pass #どちらもある場合
